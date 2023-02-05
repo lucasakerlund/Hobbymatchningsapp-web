77cc95f4e7ff752deb1808259a38ec4192727dad
lucasakerlund <lucas2001@hotmail.se>
2023-02-05 02:23:54 +0100

    Lade till så man skickas till loggning vid error 401

--- src/app/services/http.interceptor.ts
+++ src/app/services/http.interceptor.ts
@@ -5,15 +5,18 @@ import {
   HttpEvent,
   HttpInterceptor,
   HttpHeaders,
+  HttpErrorResponse,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
+import { Router } from '@angular/router';
+import { ToastService } from './toast.service';
 
 @Injectable()
 export class Httpinterceptor implements HttpInterceptor {
-  constructor() {}
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler
-  ): Observable<HttpEvent<unknown>> {
+  constructor(private router: Router, private toastService: ToastService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     if (sessionStorage.getItem('token')) {
       request = request.clone({
         setHeaders: {
@@ -21,6 +24,15 @@ export class Httpinterceptor implements HttpInterceptor {
         },
       });
     }
-    return next.handle(request);
+    return next.handle(request).pipe(catchError((error: HttpErrorResponse) => {
+      if(error.status === 401){
+        this.router.navigate(['login']);
+        if(sessionStorage.getItem('token')) {
+          this.toastService.show('Du har loggats ut, på grund av inaktivitet', {classname: 'bg-danger text-light', delay: 3000});
+          sessionStorage.clear();
+        }
+      }
+      return throwError(() => error);
+    }));
   }
 }
